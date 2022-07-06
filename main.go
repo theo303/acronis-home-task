fd316c073c1ec2a709d93e787e88965f5887c606
theo303 <theo.coutant32@gmail.com>
2022-07-06 17:36:28 +0200

    automatic folder creation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"client/filereader"
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -15,12 +16,33 @@ func main() {
 	}
 
 	fileReaderMgr := filereader.FileReadersManager{}
-	fileReaderMgr.Initialize(uri)
+	err := fileReaderMgr.Initialize(uri)
+	if err != nil {
+		panic(fmt.Errorf("error initializing fileReaderManager: %v", err))
+	}
 
 	filesToStore := fileReaderMgr.Process('A')
 
-	for filename, content := range filesToStore {
-		os.WriteFile("downloads/"+filename, content, 0644)
+	err = storeFiles(filesToStore, "downloads")
+	if err != nil {
+		panic(fmt.Errorf("error storing files: %v", err))
 	}
 	fmt.Printf("exec time: %dms\n", time.Since(startTime).Milliseconds())
+}
+
+func storeFiles(filesToStore map[string][]byte, path string) error {
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		errDir := os.Mkdir(path, os.ModePerm)
+		if errDir != nil {
+			return fmt.Errorf("creating directory: %w", errDir)
+		}
+	}
+
+	for filename, content := range filesToStore {
+		errWrite := os.WriteFile("downloads/"+filename, content, 0644)
+		if errWrite != nil {
+			return fmt.Errorf("writing file %s: %w", filename, errWrite)
+		}
+	}
+	return nil
 }
